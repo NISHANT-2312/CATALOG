cc3131da7c7cedac624c040216b43ff0efb848fa
NISHANT-2312 <nishantvaghasiya2312@gmail.com>
2022-07-29 15:36:54 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
